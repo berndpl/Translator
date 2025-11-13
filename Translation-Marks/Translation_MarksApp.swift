ac825fc5099ef97f24335a1ef9693d3388df71ef
Bernd Plontsch <bernd.plontsch@gmail.com>
2025-11-13 18:15:41 +0900

    Initial Commit

---
+++ Translation-Marks/Translation_MarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Translation_MarksApp.swift
+//  Translation-Marks
+//
+//  Created by Bernd Plontsch on 13.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct Translation_MarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
